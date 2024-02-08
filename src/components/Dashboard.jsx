a577932d3a6b42f87f968b0d6d4b16e810c0041a
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2024-02-08 15:34:18 +0530

    Changes in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='Container'>
+      <div className='sections'>
+        <p className='sectionHeading'>Join Classroom</p>
+        <input type="text" placeholder='Enter code' className='input'/>
+        <button className='buttons'>Join</button>
+      </div>
+      <div className='sections'>
+        <span className='sectionHeading'>Create Classroom</span>
+        <button className='buttons'>Create</button>
+      </div>
+    </div>
   )
 }
 
