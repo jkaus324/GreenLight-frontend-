755f263b8cab7b79c675b415229a32363bf9e6da
darkmoon-6524 <gunnishmehta2003@gmail.com>
2024-02-08 16:51:06 +0530

    Made changes in dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,102 @@
-import React from 'react'
-import './Dashboard.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Dashboard.css';
 
 const Dashboard = () => {
+  const [joinedClasses, setJoinedClasses] = useState([]);
+  const [createdClasses, setCreatedClasses] = useState([]);
+  const [classCode, setClassCode] = useState('');
+  const [className, setClassName] = useState('');
+
+  useEffect(() => {
+    axios.get('/classes')
+      .then(response => {
+        setJoinedClasses(response.data.joinedClasses);
+        setCreatedClasses(response.data.createdClasses);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  async function joinSubmitHandler(e) {
+    e.preventDefault();
+
+    try {
+      // await axios.post('/joinClassroom', { classCode });
+      console.log('Successfully joined classroom with code:', classCode);
+      // const response = await axios.get('/classes');
+      setJoinedClasses([{id: classCode, name: 'demo'}]);  // change the name from demo to response.data.name
+    } catch (error) {
+      console.error('Error joining classroom: ', error);
+    }
+  }
+
+  async function createSubmitHandler(e) {
+    e.preventDefault();
+
+    try {
+      // const response = await axios.post('/createClassroom', { className });
+      const className = 'demo';
+      console.log('Successfully created classroom with name:', className);
+      setCreatedClasses([{ id: "0000", name: className}]);  // response.data.classes
+    } catch (error) {
+      console.error('Error creating classroom: ', error);
+    }
+  }
+
+  function handleCodeInputChange(event) {
+    setClassCode(event.target.value);
+  }
+
+  function handleNameInputChange(event) {
+    setClassName(event.target.value);
+  }
+
   return (
     <div className='Container'>
       <div className='sections'>
         <p className='sectionHeading'>Join Classroom</p>
-        <input type="text" placeholder='Enter code' className='input'/>
-        <button className='buttons'>Join</button>
+        <input
+          type="text"
+          placeholder='Enter code'
+          className='input'
+          value={classCode}
+          onChange={handleCodeInputChange}
+        />
+        <button className='buttons' onClick={joinSubmitHandler}>Join</button>
+        <div>
+          <h3>Joined Classes: </h3>
+          <ul>
+            {console.log(joinedClasses)}
+            {joinedClasses?.map(classItem => (
+              <li key={classItem.id}>{classItem.id}  {classItem.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
+
       <div className='sections'>
-        <span className='sectionHeading'>Create Classroom</span>
-        <button className='buttons'>Create</button>
+        <p className='sectionHeading'>Create Classroom</p>
+        <input
+          type="text"
+          placeholder='Enter class name'
+          className='input'
+          value={className}
+          onChange={handleNameInputChange}
+        />
+        <button className='buttons' onClick={createSubmitHandler}>Create</button>
+        <div>
+          <h3>Created Classes:</h3>
+          <ul>
+            {createdClasses?.map(classItem => (
+              <li key={classItem.id}>{classItem.id}  {classItem.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
