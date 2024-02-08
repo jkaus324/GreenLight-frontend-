8324049def08041fb2586bdd25d43436216ee8e1
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2024-02-08 10:25:40 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Classroom from './components/Classroom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/classroom' element={<Classroom/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
